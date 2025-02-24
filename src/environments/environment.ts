aca633b3015869881a2132d94939fac3cca9b7d5
Janine Franken <janine@olconet.com>
2025-02-24 18:16:10 +0100

    port angepasst für backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5455',
+  apiUrl: 'http://localhost:3001',
   apiUrlSelf: 'http://localhost:4200',
   defaultSignature: 'JanineFranken',
   defaultEmail: 'janine@automoto-sr.info',
