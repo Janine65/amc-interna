95282a47a184b5d45c35868a1dd3b45b5361316a
Janine Franken <janine@olconet.com>
2026-06-01 17:50:53 +0200

    fix: close att-show

--- src/app/components/buchhaltung/attachement-show/attachement-show.component.ts
+++ src/app/components/buchhaltung/attachement-show/attachement-show.component.ts
@@ -6,17 +6,20 @@ import {
   signal,
 } from '@angular/core';
 import { BackendService } from '@app/service';
-import { DynamicDialogConfig } from 'primeng/dynamicdialog';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-attachement-show',
   templateUrl: './attachement-show.component.html',
   styleUrls: ['./attachement-show.component.scss'],
+  imports: [ButtonModule],
 })
 export class AttachementShowComponent implements AfterViewInit, OnDestroy {
   private backendService = inject(BackendService);
   config = inject(DynamicDialogConfig);
+  ref = inject(DynamicDialogRef);
   private sanitizer = inject(DomSanitizer);
 
   receipt = '';
